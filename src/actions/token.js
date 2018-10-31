38bdf2208b50f6983fc0483e3862a66256a80354
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-10-31 15:01:07 -0300

    fix(t2cr): remove unsused resources, fix conflict in naming

--- src/actions/token.js
+++ src/actions/token.js
@@ -1,8 +1,6 @@
 import { createActions } from 'lessdux'
 
 /* Actions */
-
-// Tokens
 export const tokens = createActions('TOKENS')
 
 // Token
@@ -10,43 +8,13 @@ export const token = {
   ...createActions('TOKEN', {
     withCreate: true,
     withUpdate: true
-  }),
-  EXECUTE_REQUEST: 'EXECUTE_TOKEN_REQUEST',
-  CHALLENGE_REQUEST: 'CHALLENGE_REQUEST',
-  APPEAL_RULING: 'APPEAL_TOKEN_RULING',
-  EXECUTE_RULING: 'EXECUTE_TOKEN_RULING'
+  })
 }
 
 /* Action Creators */
 
-// Tokens
-export const fetchTokens = (cursor, count, filterValue, sortValue) => ({
-  type: tokens.FETCH,
-  payload: { cursor, count, filterValue, sortValue }
-})
-
 // Token
-export const createToken = imageFileDataURL => ({
+export const createToken = tokenData => ({
   type: token.CREATE,
-  payload: { imageFileDataURL }
-})
-export const fetchToken = (ID, withDisputeData) => ({
-  type: token.FETCH,
-  payload: { ID, withDisputeData }
-})
-export const executeTokenRequest = ID => ({
-  type: token.EXECUTE_REQUEST,
-  payload: { ID }
-})
-export const submitTokenChallenge = ID => ({
-  type: token.SUBMIT_CHALLENGE,
-  payload: { ID }
-})
-export const appealTokenRuling = ID => ({
-  type: token.APPEAL_RULING,
-  payload: { ID }
-})
-export const executeTokenRuling = ID => ({
-  type: token.EXECUTE_RULING,
-  payload: { ID }
+  payload: { token: tokenData }
 })
--- src/actions/wallet.js
+++ src/actions/wallet.js
@@ -8,12 +8,6 @@ export const accounts = createActions('ACCOUNTS')
 // Balance
 export const balance = createActions('BALANCE')
 
-// Settings
-export const settings = {
-  ...createActions('SETTINGS', { withUpdate: true }),
-  UPDATE_EMAIL: 'UPDATE_SETTINGS_EMAIL'
-}
-
 /* Action Creators */
 
 // Accounts
@@ -21,9 +15,3 @@ export const fetchAccounts = () => ({ type: accounts.FETCH })
 
 // Balance
 export const fetchBalance = () => ({ type: balance.FETCH })
-
-// Settings
-export const updateEmail = ({ email }) => ({
-  type: settings.UPDATE_EMAIL,
-  payload: { email }
-})
