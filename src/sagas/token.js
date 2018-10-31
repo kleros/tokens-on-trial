ffe272b8afbf6a3dd86d40eea5b3fe710ba04e85
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-10-31 15:37:16 -0300

    feat(t2cr): add fetch token list generator

--- src/sagas/token.js
+++ src/sagas/token.js
@@ -1,8 +1,28 @@
 import { takeLatest, call } from 'redux-saga/effects'
 
-import * as tokenActions from '../actions/token'
 import { lessduxSaga } from '../utils/saga'
-import { PATCH_TOKEN_URL } from '../bootstrap/dapp-api'
+import { PATCH_TOKEN_URL, arbitrableTokenList } from '../bootstrap/dapp-api'
+import * as tokenActions from '../actions/token'
+
+/**
+ * Fetches a token from the list.
+ * @param {{ type: string, payload: ?object, meta: ?object }} action - The action object.
+ * @returns {object} - The fetched token.
+ */
+export function* fetchToken({ payload: { ID } }) {
+  const token = yield call(arbitrableTokenList.methods.items(ID).call)
+
+  return {
+    ID,
+    status: token.status,
+    balance: String(token.balance),
+    challengeReward: String(token.balance),
+    latestAgreementID: String(token.latestAgreementID),
+    lastAction: token.lastAction
+      ? new Date(Number(token.lastAction * 1000))
+      : null
+  }
+}
 
 /**
  * Submits a token to the list.
