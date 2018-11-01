d867eb044e55f8abcf31cdfd0eb1bf78d53f079e
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-11-01 17:39:57 -0300

    feat(card): add check and etherscan link

--- src/components/token-card/index.js
+++ src/components/token-card/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Img from 'react-image'
 import PropTypes from 'prop-types'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
 import './token-card.css'
+import EtherScanLogo from '../../../src/assets/images/etherscan.png'
 
 const TokenCard = ({ name, imageUrl, ticker, badges }) => (
   <div className="TokenCard">
     <div className="TokenCard-header">
+      <FontAwesomeIcon icon="check" color="#11BABD" />
       <h5>
         {name} - {ticker}
       </h5>
+      <a href="/">
+        <Img src={EtherScanLogo} />
+      </a>
     </div>
     <div className="TokenCard-content">
       <Img
@@ -26,7 +32,7 @@ TokenCard.propTypes = {
   name: PropTypes.string.isRequired,
   imageUrl: PropTypes.string.isRequired,
   ticker: PropTypes.string.isRequired,
-  badges: PropTypes.shape({}).isRequired
+  badges: PropTypes.shape([]).isRequired
 }
 
 export default TokenCard
