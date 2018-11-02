5688227e6f29d4f24d4e83f723a969605d4530fb
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-11-02 15:43:44 -0300

    feat(filter): add filter options selectors

--- src/components/filter-bar/index.js
+++ src/components/filter-bar/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 
+import Button from '../button'
+
 import './filter-bar.css'
 
 const FilterBar = () => (
@@ -17,6 +19,61 @@ const FilterBar = () => (
       </div>
     </div>
     <hr className="FilterBar-separator" />
+    <div className="FilterBar-filterSelect">
+      <div className="FilterBar-my">
+        <Button className="FilterBar-my-button" size="small" type="secondary">
+          My Submissions
+        </Button>
+        <Button className="FilterBar-my-button" size="small" type="secondary">
+          My Challenges
+        </Button>
+      </div>
+      <div className="FilterBar-status">
+        <Button
+          className="FilterBar-status-button FilterBar-status-button-small"
+          size="small"
+          type="primary"
+        >
+          Registered
+        </Button>
+        <Button
+          className="FilterBar-status-button FilterBar-status-button-medium"
+          size="small"
+          type="secondary"
+        >
+          Registration Requests
+        </Button>
+        <Button
+          className="FilterBar-status-button FilterBar-status-button-large"
+          size="small"
+          type="secondary"
+        >
+          Challenged Registration Requests
+        </Button>
+        <Button
+          className="FilterBar-status-button FilterBar-status-button-small"
+          size="small"
+          type="secondary"
+        >
+          Cleared
+        </Button>
+        <Button
+          className="FilterBar-status-button FilterBar-status-button-medium"
+          size="small"
+          type="secondary"
+        >
+          Clearing Requests
+        </Button>
+        <Button
+          className="FilterBar-status-button FilterBar-status-button-large"
+          size="small"
+          type="secondary"
+        >
+          Challenged Clearing Requests
+        </Button>
+      </div>
+    </div>
+    <hr className="FilterBar-separator-neon" />
     <div className="FilterBar">
       <div className="FilterBar-count">123.456 Tokens</div>
       <div className="FilterBar-sort">
