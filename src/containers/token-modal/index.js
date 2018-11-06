240b9fb51ec73585fae3a817ad3f16bdd6951511
Matheus Faria de Alencar <mtsalenc@gmail.com>
2018-11-06 18:24:00 -0200

    feat(submit): prepare component for token submission

--- src/containers/token-modal/index.js
+++ src/containers/token-modal/index.js
@@ -1,35 +1,84 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
 import * as modalActions from '../../actions/modal'
 import * as modalSelectors from '../../reducers/modal'
+import * as tokenActions from '../../actions/token'
+import * as arbitrableTokenListActions from '../../actions/arbitrable-token-list'
+import * as arbitrableTokenListSelectors from '../../reducers/arbitrable-token-list'
 import Modal from '../../components/modal'
 
 import Submit from './components/submit'
+import {
+  getTokenFormIsInvalid,
+  submitTokenForm
+} from './components/submit/token-form'
 
 import './token-modal.css'
 
-const TokenModal = ({ openTokenModal, closeTokenModal }) => (
-  <Modal
-    isOpen={openTokenModal !== null}
-    onRequestClose={closeTokenModal}
-    className="TokenModal"
-  >
-    <Submit />
-  </Modal>
-)
+class TokenModal extends PureComponent {
+  static propTypes = {
+    tokenFormIsInvalid: PropTypes.bool.isRequired,
+    openTokenModal: modalSelectors.openTokenModalShape,
+    arbitrableTokenListData:
+      arbitrableTokenListSelectors.arbitrableTokenListDataShape.isRequired,
 
-TokenModal.propTypes = {
-  closeTokenModal: PropTypes.func.isRequired,
-  openTokenModal: modalSelectors.openTokenModalShape
-}
+    closeTokenModal: PropTypes.func.isRequired,
+    fetchArbitrableTokenListData: PropTypes.func.isRequired,
+    submitTokenForm: PropTypes.func.isRequired
+  }
 
-TokenModal.defaultProps = {
-  openTokenModal: null
+  static defaultProps = {
+    openTokenModal: null
+  }
+
+  handleSubmitTokenClick = token => {
+    console.info('clicked', token)
+  }
+
+  componentDidMount() {
+    const { fetchArbitrableTokenListData } = this.props
+    fetchArbitrableTokenListData()
+  }
+
+  render() {
+    const {
+      openTokenModal,
+      closeTokenModal,
+      arbitrableTokenListData,
+      submitTokenForm,
+      tokenFormIsInvalid
+    } = this.props
+    return (
+      <Modal
+        isOpen={openTokenModal !== null}
+        onRequestClose={closeTokenModal}
+        className="TokenModal"
+      >
+        <Submit
+          arbitrableTokenListData={arbitrableTokenListData}
+          closeTokenModal={closeTokenModal}
+          submitTokenForm={submitTokenForm}
+          submitToken={this.handleSubmitTokenClick}
+          tokenFormIsInvalid={tokenFormIsInvalid}
+        />
+      </Modal>
+    )
+  }
 }
 
 export default connect(
-  state => ({ openTokenModal: state.modal.openTokenModal }),
-  { closeTokenModal: modalActions.closeTokenModal }
+  state => ({
+    openTokenModal: state.modal.openTokenModal,
+    tokenFormIsInvalid: getTokenFormIsInvalid(state),
+    arbitrableTokenListData: state.arbitrableTokenList.arbitrableTokenListData
+  }),
+  {
+    closeTokenModal: modalActions.closeTokenModal,
+    submitToken: tokenActions.createToken,
+    submitTokenForm,
+    fetchArbitrableTokenListData:
+      arbitrableTokenListActions.fetchArbitrableTokenListData
+  }
 )(TokenModal)
