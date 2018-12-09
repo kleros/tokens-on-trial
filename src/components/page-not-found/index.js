d2f7f85cc00ccb94b8f4a735043b1aed14641dfc
Matheus Alencar <mtsalenc@gmail.com>
2018-12-09 17:31:11 -0200

    fix(style): not found and token details

--- src/components/page-not-found/index.js
+++ src/components/page-not-found/index.js
@@ -5,7 +5,11 @@ import './page-not-found.css'
 export default () => (
   <div className="PageNotFound">
     <div className="PageNotFound-message">
-      404. This is not the page you are looking for.
+      404.
+      <br />
+      <small>
+        <h3>This is not the page you are looking for.</h3>
+      </small>
     </div>
   </div>
 )
