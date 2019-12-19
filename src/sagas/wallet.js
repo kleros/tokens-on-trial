2dfa8c056837ddc5bcdac7d51f27289945328881
Matheus Alencar <malencar@pm.me>
2019-12-19 11:09:52 -0300

    fix: swap ethereum.enable() for eth_requestAccount

--- src/sagas/wallet.js
+++ src/sagas/wallet.js
@@ -13,7 +13,7 @@ function* fetchAccounts() {
   if (window.ethereum) {
     const { web3 } = yield call(instantiateEnvObjects)
 
-    yield call(window.ethereum.enable)
+    yield window.ethereum.send({ method: 'eth_requestAccounts' })
     return yield call(web3.eth.getAccounts)
   } else return []
 }
